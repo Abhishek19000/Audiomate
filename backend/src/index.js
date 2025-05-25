069de31b757739a00915322af0cd615e03e22bcb
Abhishek19000 <abhiraj19000@gmail.com>
2025-05-25 22:38:46 +0530

    Routes added

---
+++ backend/src/index.js
@@ -0,0 +1,36 @@
+import express from 'express';
+import dotenv from 'dotenv';
+import userRoutes from "./routes/user.route.js";
+import adminRoutes from "./routes/admin.route.js";
+import authRoutes from "./routes/auth.route.js";
+import songRoutes from "./routes/song.route.js";
+import albumRoutes from "./routes/album.route.js";
+import statRoutes from "./routes/stat.route.js";
+
+
+dotenv.config();
+
+const app= express();
+
+app.use(express.json());
+
+const port=process.env.PORT;
+
+app.use("/api/v1/users",userRoutes);
+app.use("/api/v1/admin",adminRoutes);
+app.use("/api/v1/auth",authRoutes);
+app.use("/api/v1/songs",songRoutes);
+app.use("/api/v1/albums",albumRoutes);
+app.use("/api/v1/stats",statRoutes);
+
+app.post("/",(req,res)=>{
+    const re11=req.body;
+
+    console.log(re11);
+
+    res.send(re11)
+});
+
+app.listen(port,()=>{
+    console.log("Server running "+ process.env.PORT);
+});
---
+++ backend/src/routes/album.route.js
@@ -0,0 +1,10 @@
+import { Router } from "express";
+
+const router=Router();
+
+router.get("/",(req,res)=>
+{
+    res.send("Hello from album");
+})
+
+export default router
