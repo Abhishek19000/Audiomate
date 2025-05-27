b69a72ffbd3d3cb21070e2f9698a5e7b72b1036c
Abhishek19000 <abhiraj19000@gmail.com>
2025-05-27 14:09:50 +0530

    Signup Logic implemented in auth controller file.

---
+++ backend/src/controllers/auth.controller.js
@@ -0,0 +1,27 @@
+import {User} from "../models/User";
+
+export const authCallback = async (req,res)=>
+{
+    try
+    {
+        const {id, firstName, lastName, imageUrl}=req.body;
+
+        //Check if the user already exists in the database or not
+        const user= await User.findOne({clerkId:id});
+        if(!user)
+        {
+            //signup
+            await User.create({
+                clerkId:id,
+                fullName: `${firstName} ${lastName}`,
+                imageUrl:imageUrl
+            })
+        }
+        res.status(200).json({success:true});
+    }
+    catch(error)
+    {
+        console.log("Error in auth callback", error);
+        res.status(500).json({message:"Internak Server Error",error,success:false});
+    }
+}
