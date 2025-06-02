3ece280f9f1887780711f5bd4f05ef979c508c0d
Abhishek19000 <abhiraj19000@gmail.com>
2025-06-02 23:14:14 +0530

    feat: add /sso-callback route for handling SSO redirect

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,19 +1,18 @@
-import { SignedIn, SignedOut, SignInButton, UserButton } from '@clerk/clerk-react';
-import { Button } from './components/ui/button';
+import { Route, Routes } from "react-router-dom";
+import AuthCallbackPage from "./pages/auth-callback/AuthCallbackPage";
+import HomePage from "./pages/home/HomePage";
+import { AuthenticateWithRedirectCallback } from "@clerk/clerk-react";
 
 export default function App() {
   return (
     <>
-    <header>
-      <SignedOut>
-        <SignInButton>
-          <Button>Sign In</Button>
-        </SignInButton>
-      </SignedOut>
-      <SignedIn>
-        <UserButton />
-      </SignedIn>
-    </header>
+    <Routes>
+      <Route path="/" element={<HomePage />} />
+      <Route path="/sso-callback" element={<AuthenticateWithRedirectCallback 
+      signInForceRedirectUrl={"/auth-callback"}
+      />}></Route>
+      <Route path="/auth-callback" element={<AuthCallbackPage/>} />
+    </Routes>
     </>
   );
 }
