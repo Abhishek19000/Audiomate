1642abd262d7782bee642259445726db9b6823db
Abhishek19000 <abhiraj19000@gmail.com>
2025-06-02 23:17:47 +0530

    feat: add Google OAuth login button with redirect configuration

---
+++ frontend/src/components/SignInOAuthButtons.tsx
@@ -0,0 +1,27 @@
+import { useSignIn } from "@clerk/clerk-react"
+import { Button } from "./ui/button";
+
+function SignInOAuthButtons() {
+    const {signIn,isLoaded}=useSignIn();
+
+    console.log(isLoaded)
+
+    if(!isLoaded)
+    {
+        return null
+    }
+    const singInWithGoogle= ()=>{
+        signIn.authenticateWithRedirect({
+            strategy: "oauth_google",
+            redirectUrl:"/sso-callback",
+            redirectUrlComplete:"/auth-callback",
+        })
+    }
+  return (
+    <Button onClick={singInWithGoogle} variant={"secondary"} className="w-full text-white border-zinc-200 h-11">
+        Continue with Google
+    </Button>
+  )
+}
+
+export default SignInOAuthButtons
